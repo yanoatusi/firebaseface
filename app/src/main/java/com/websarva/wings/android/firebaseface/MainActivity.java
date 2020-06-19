9f60ecb4bc81a2671067a4698d0dfaa0dfb88dd9
yanoatusi <rG734jQyup2A@outlook.jp>
2020-06-19 12:17:34 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/firebaseface/MainActivity.java
@@ -0,0 +1,14 @@
+package com.websarva.wings.android.firebaseface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
